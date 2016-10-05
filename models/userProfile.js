d8b5bfda89325159c6f4b9b1a195449956532c4e
John Green <johngreen@Johns-MacBook-Pro.local>
2016-10-05 12:49:58 -0600

    Wednesday middle of day push

--- models/userProfile.js
+++ models/userProfile.js
@@ -1,13 +1,11 @@
 var mongoose = require('mongoose');
 
 var userProfileSchema = mongoose.Schema({
-    name:               {type: String},
-    email:              {type: String},
-    password:           {type: String},
-    student:            {type: Boolean},
-    teacher:            {type: Boolean},
-    homeGolfCourse:     {type: Array, default: []},
-    refID:              {type: String},
+    name: { type: String },
+    email: { type: String },
+    password: { type: String },
+    userType: { type: String },
+    refID: { type: String },
 });
 
 module.exports = mongoose.model('userProfile', userProfileSchema);
